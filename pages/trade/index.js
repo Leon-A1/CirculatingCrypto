c42c59832839b0da5febfe6336ae0bfb06889b0d
Leon-A1 <leonaviev@gmail.com>
2021-09-10 22:26:53 +0300

    update

--- pages/trade/index.js
+++ pages/trade/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "../../components/Layout";
 import DashboardLayout from "../../components/DashboardLayout";
 import { Store } from "../../utils/Store";
